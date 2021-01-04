27bae085f192920680b5a5f5d6bfda141f1d1003
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2021-01-04 15:30:44 -0800

    test commit

---
+++ src/main/java/com/warframepda/www/WarframepdaApplication.java
@@ -0,0 +1,13 @@
+package com.warframepda.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarframepdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarframepdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warframepda/www/WarframepdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warframepda.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarframepdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
