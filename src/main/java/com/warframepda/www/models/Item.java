a7b734219adc922d9717583134ea34604242be46
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2021-01-04 17:22:07 -0800

    Created models for database

---
+++ src/main/java/com/warframepda/www/models/Item.java
@@ -0,0 +1,67 @@
+package com.warframepda.www.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long itemid;
+
+    @Column(nullable = false)
+    private String itemname;
+
+    private String imageurl;
+
+    @OneToMany(mappedBy = "item",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    @JsonIgnoreProperties(value = "item", allowSetters = true)
+    private List<Part> parts = new ArrayList<>();
+
+    public Item() {
+    }
+
+    public Item(String itemname, String imageurl) {
+        this.itemname = itemname;
+        this.imageurl = imageurl;
+    }
+
+    public long getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(long itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public List<Part> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<Part> parts) {
+        this.parts = parts;
+    }
+}
---
+++ src/main/java/com/warframepda/www/models/Part.java
@@ -0,0 +1,69 @@
+package com.warframepda.www.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "parts")
+public class Part {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long partid;
+
+    private String partname;
+
+    @ManyToOne
+    @JoinColumn(name = "itemid",
+            nullable = false)
+    @JsonIgnoreProperties(value = "parts")
+    private Item item;
+
+
+    @OneToMany(mappedBy = "part", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties(value = "part", allowSetters = true)
+    private List<Order> orders = new ArrayList<>();
+
+    public Part() {
+    }
+
+    public Part(String partname, Item item) {
+        this.partname = partname;
+        this.item = item;
+    }
+
+    public long getPartid() {
+        return partid;
+    }
+
+    public void setPartid(long partid) {
+        this.partid = partid;
+    }
+
+    public String getPartname() {
+        return partname;
+    }
+
+    public void setPartname(String partname) {
+        this.partname = partname;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
